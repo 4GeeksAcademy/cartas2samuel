c444d232a42520c01272e9cafa2ea4f7fc91a281
Jsamuel1919 <156345975+Jsamuel1919@users.noreply.github.com>
2024-03-05 19:16:41 +0000

    segundo intento de este proyecto con estilos

--- src/app.js
+++ src/app.js
@@ -9,3 +9,18 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let palos = ["♦", "♥", "♠", "♣️ "];
+let cartas = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+let i = document.getElementById("iconoArriba");
+let j = document.getElementById("numeroCarta");
+let x = document.getElementById("iconoAbajo");
+
+function visualizarCartas(palos, cartas) {
+  let baraja = palos[Math.floor(Math.random() * palos.length)];
+  let numeros = cartas[Math.floor(Math.random() * cartas.length)];
+  i.textContent = baraja;
+  j.textContent = numeros;
+  x.textContent = baraja;
+}
+visualizarCartas(palos, cartas);
